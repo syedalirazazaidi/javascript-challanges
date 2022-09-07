14014362727e8f00d088ed489c74f134211378ea
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2022-09-08 01:24:09 +0500

    add array problems

--- logic.js
+++ logic.js
@@ -1,2 +1,16 @@
 // sum of diagnol
 
+var arry = [
+  [1, 2, 3],
+  [3, 4, 5],
+  [5, 6, 6],
+]
+let sum = 0
+for (let index = 0; index < arry.length; index++) {
+  for (var i = 0; i < arry.length; i++) {
+    if (i === index) {
+      sum += arry[i][index]
+    }
+  }
+}
+console.log(sum)
